1e6726c228721d9512b5109d0d24e68d1c2c9620
leonid <chemestru@gmail.com>
2023-12-29 16:22:10 +0300

    Update

---
+++ domain_health_check/go.mod
@@ -0,0 +1,5 @@
+module github.com/Leonid-Sarmatov/golang-cheat-sheet
+
+go 1.20
+
+require github.com/urfave/cli/v2
---
+++ domain_health_check/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+	"fmt"
+)
+
+func main() {
+	app := &cli.App {
+		Name: "Healthchecker",
+		Usage: "A tiny tool for checks whether a website is running or is down",
+		Flags: []cli.Flags {
+			&cli.StringFlag {
+				Name: "domain",
+				Aleases: []string{"d"},
+				Usage: "Domain name to chek.",
+				Required: true,
+			},
+			&cli.StringFlag {
+				Name: "port",
+				Aleases: []string{"p"},
+				Usage: "Port number to check.",
+				Required: false,
+			},
+		},
+		Actions: func(c *cli.Context) error {
+			port := c.String("port")
+			if port == "" {
+				port = "80"
+			}
+			status := Check(c.String("domain"), port)
+			fmt.Println(status)
+			return nil
+		},
+	}
+}
